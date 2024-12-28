cac9bd0141824e9be243f5976e4e6c920c2887a4
palashj3 <palash.jain.cw@gmail.com>
2024-12-28 13:46:41 +0530

    Add Logging, Basic Ollama Chat Class

---
+++ src/localllmchatbot/LocalModelChat/LocalModelChat.py
@@ -0,0 +1,49 @@
+from typing import Callable
+from ollama import chat
+from localllmchatbot.logger_setup import loguru_setup
+
+logger = loguru_setup()
+
+
+class LocalModelChat:
+    def __init__(
+        self,
+        model: str,
+        base_url: str,
+        system_prompt: str,
+        tools: list[Callable],
+    ):
+        logger.info(
+            f"Initializing ChatOllama with model: {model}, base_url: {base_url}, system_prompt: {system_prompt}, and tools: {tools}"
+        )
+        self.model = model
+        self.base_url = base_url
+        self.system_prompt = system_prompt
+        self.tools = tools
+        self.message_history = [
+            {
+                "role": "system",
+                "content": self.system_prompt,
+            },
+        ]
+
+    def _generate_chat_response(self, prompt):
+        logger.info(f"Generating chat response for prompt: {prompt}")
+        self.message_history.append({"role": "user", "content": prompt})
+        response = chat(
+            model=self.model,
+            messages=self.message_history,
+        )
+        self.message_history.append(
+            {"role": "assistant", "content": response.message.content}
+        )
+        return response
+
+    def _clear_message_history(self):
+        logger.info("Clearing message history")
+        self.message_history = [
+            {
+                "role": "system",
+                "content": self.system_prompt,
+            },
+        ]
---
+++ src/localllmchatbot/logger_setup.py
@@ -0,0 +1,25 @@
+from loguru import logger
+import sys
+
+
+def loguru_setup(
+    logfile="/Users/palashjain/Projects/LLMApps/ROBIN/logs/logfile_{time:YYYY-MM-DD}.log",
+    rotation="12:00",
+):
+    log_format = "{time} | {level} | {name} | {line} | {message}"
+    logger.remove()
+    logger.add(
+        logfile,
+        colorize=True,
+        format=log_format,
+        level="DEBUG",
+    )
+    logger.add(
+        sys.stderr,
+        format=log_format,
+        colorize=True,
+        backtrace=True,
+        diagnose=True,
+    )
+
+    return logger
